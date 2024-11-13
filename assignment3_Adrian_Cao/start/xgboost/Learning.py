e28f4f93ee9e645689fa7de6a8e31ef932d39e39
adriancmx <adrian.c@wustl.edu>
2024-11-13 01:40:42 -0600

    path

--- assignment3_Adrian_Cao/start/xgboost/Learning.py
+++ assignment3_Adrian_Cao/start/xgboost/Learning.py
@@ -6,7 +6,7 @@ import pandas as pd
 import joblib
 
 # Define paths using environment variables or default values
-data_path = os.getenv('DATA_PATH', 'start/public/health_data.csv')
+data_path = os.getenv('DATA_PATH', 'health_data.csv')
 model_path = os.getenv('MODEL_PATH', 'xgboost_model.json')
 scaler_path = os.getenv('SCALER_PATH', 'scaler.pkl')
 
